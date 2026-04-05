f3bc63252a6bd5dcb6aa95c8ae5606ce56781bf7
Ahmed Ameur Kh <ahmed.ameur.khanfir@gmail.com>
2026-04-05 11:10:29 +0100

    redoing everything

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,8 @@
-import Hero from '@/components/home/Hero'
-import Features from '@/components/home/Features'
-import CTA from '@/components/home/CTA'
-import Navbar from '@/components/layout/Navbar'
 
 export default function HomePage() {
   return (
     <main className="flex flex-col min-h-screen gap-32 pt-20">
-      <Hero />
-      <Features />
-<div className="h-32 bg-gradient-to-b from-transparent to-black/20" />
-
-      {/* Push CTA to bottom if content is short */}
-      <div className="flex-grow" />
-
-      <CTA />
+      <p>Redoing everything</p>
     </main>
   )
 }
